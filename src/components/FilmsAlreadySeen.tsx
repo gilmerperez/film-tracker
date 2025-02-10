41ef3eb588493186d52cfb0fbc2fb93f52704406
gilmerperez <gilmer2002@outlook.com>
2025-02-09 21:37:31 -0500

    Finished FilmsAlreadySeen.tsx

--- src/components/FilmsAlreadySeen.tsx
+++ src/components/FilmsAlreadySeen.tsx
@@ -1,14 +1,26 @@
 import type React from 'react';
 // TODO: Uncomment when Film interface is added
-// import type Film from '../utils/interfaces/Film.interface';
+import type Film from '../utils/interfaces/Film.interface';
 import FilmCard from './FilmCard';
 
 // TODO: Define seenFilmProps
-// interface seenFilmProps {
-// }
+interface seenFilmProps {
+  alreadyWatchedFilms: Film[];
+  removeFromStorage:
+    | ((
+        e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+        currentlyOnWatchList: boolean | null | undefined,
+        currentlyOnSeenItList: boolean | null | undefined,
+        title: string | null
+      ) => void)
+    | null;
+}
 
 // TODO: Destructure alreadyWatchedFilms from props
-const FilmsAlreadySeen = (props) => {
+const FilmsAlreadySeen = ({
+  alreadyWatchedFilms,
+  removeFromStorage,
+}: seenFilmProps) => {
   return (
     <ul>
       {alreadyWatchedFilms.map((film) => (
