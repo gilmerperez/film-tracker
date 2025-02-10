6b435b481368f702a46f459ca900755d53397a82
gilmerperez <gilmer2002@outlook.com>
2025-02-09 21:26:58 -0500

    Added Film interface for OMDb API

--- src/utils/interfaces/Film.interface.tsx
+++ src/utils/interfaces/Film.interface.tsx
@@ -1,4 +1,11 @@
 // TODO: Define an interface for the `Film` object based on the data provided by OMDb API
 // TODO: Can the properties be marked as readonly?
-// export default interface Film {
-// }
+export default interface Film {
+    readonly Title: string | null;
+    readonly Director: string | null;
+    readonly Actors: string | null;
+    readonly Released: string | null;
+    readonly Poster: string | null;
+    readonly Genre: string | null;
+    readonly Plot: string | null;
+  }
