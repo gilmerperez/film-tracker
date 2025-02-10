185bcf42c0204d352a130e554bdcd401209ff3cc
gilmerperez <gilmer2002@outlook.com>
2025-02-09 21:40:15 -0500

    Finished FilmCard.tsx

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -1,37 +1,86 @@
 import type React from 'react';
 // TODO: Uncomment when Film interface is added
-// import type Film from '../interfaces/Film.interface';
+import type Film from '../interfaces/Film.interface';
 import { IoEyeOutline } from 'react-icons/io5';
 import { ImCross } from 'react-icons/im';
 import { CgPlayListAdd } from 'react-icons/cg';
 
 // TODO: Define FilmCardProps
-// type FilmCardProps = {
-// };
+type FilmCardProps = {
+  currentFilm: Film;
+  addToWatchList?: (() => void) | null;
+  addToSeenItList?: (() => void) | null;
+  onWatchList?: boolean | null;
+  onSeenItList?: boolean | null;
+  removeFromStorage?:
+    | ((
+        e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+        currentlyOnWatchList: boolean | null | undefined,
+        currentlyOnSeenItList: boolean | null | undefined,
+        title: string | null
+      ) => void)
+    | null;
+};
 
-const FilmCard = () => {
+const FilmCard = ({
+  currentFilm,
+  addToWatchList,
+  addToSeenItList,
+  onWatchList,
+  onSeenItList,
+  removeFromStorage,
+}: FilmCardProps) => {
   return (
     <>
       {currentFilm?.Title ? (
         <section className='filmCard'>
-          <figure>{/* TODO: Add film poster*/}</figure>
+          <figure>
+            <img src={`${currentFilm.Poster}`} alt={`${currentFilm.Title}`} />
+          </figure>
           <article className='details'>
-            {/* TODO: Add film title, director, actors, released, and genre */}
+            <h2>{currentFilm.Title}</h2>
+            <p>
+              <strong>Directed By:</strong> {currentFilm.Director}
+            </p>
+            <p>
+              <strong>Starring:</strong> {currentFilm.Actors}
+            </p>
+            <p>
+              <strong>Released:</strong> {currentFilm.Released}
+            </p>
+            <p>
+              <strong>Genre:</strong> {currentFilm.Genre}
+            </p>
           </article>
-          <article className='plot'>{/* TODO: Add film plot here */}</article>
-          {/* If film is on Watch List or Seen It list film can be removed
-              Else film can be added to Watch List or Seen It list */}
+          <article className='plot'>
+            <p>
+              <strong>Plot:</strong> {currentFilm.Plot}
+            </p>
+          </article>
           {onWatchList || onSeenItList ? (
             <aside className='icons'>
-              {/* TODO: Add remove film from localStorage logic here */}
-              <ImCross style={{ fontSize: '40px', cursor: 'pointer' }} />
+              <ImCross
+                style={{ fontSize: '40px', cursor: 'pointer' }}
+                onClick={(e: React.MouseEvent<SVGSVGElement, MouseEvent>) =>
+                  removeFromStorage?.(
+                    e,
+                    onWatchList,
+                    onSeenItList,
+                    currentFilm.Title
+                  )
+                }
+              />
             </aside>
           ) : (
             <aside className='icons'>
-              {/* TODO: Implement add to WatchList logic here*/}
-              <CgPlayListAdd style={{ fontSize: '50px', cursor: 'pointer' }} />
-              {/* TODO: Implement add to Seen It list logic here*/}
-              <IoEyeOutline style={{ fontSize: '50px', cursor: 'pointer' }} />
+              <CgPlayListAdd
+                style={{ fontSize: '50px', cursor: 'pointer' }}
+                onClick={() => addToWatchList?.()}
+              />
+              <IoEyeOutline
+                style={{ fontSize: '50px', cursor: 'pointer' }}
+                onClick={() => addToSeenItList?.()}
+              />
             </aside>
           )}
         </section>
